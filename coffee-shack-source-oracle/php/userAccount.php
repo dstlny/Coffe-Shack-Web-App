e9d9f64100b20108d509eb0c00479209da4ca443
Luke Elam <lukeelam1999@icloud.com>
2019-05-09 21:25:29 +0100

    Update userAccount.php

--- coffee-shack-source-oracle/php/userAccount.php
+++ coffee-shack-source-oracle/php/userAccount.php
@@ -1,6 +1,8 @@
 <?php
 include '../pages/header.php';
 
+
+//Print either the customers information or the staff-members information if they are in the Account page.
 if(isset($_SESSION['loggedIn']) || isset($_SESSION['adminLoggedIn'])){
    
    if(isset($_SESSION['userName'])){
