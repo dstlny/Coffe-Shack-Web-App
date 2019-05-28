d4ca866539e738c56214857351b1aff276f8a270
Luke Elam <lukeelam1999@icloud.com>
2019-05-28 15:35:49 +0100

    Get rid of red font on second statement.

--- coffee-shack-source-oracle/pages/staffPage.php
+++ coffee-shack-source-oracle/pages/staffPage.php
@@ -7,7 +7,7 @@
 	    echo '<meta http-equiv="Refresh" content="15">';
 	    $obj->printCustomerOrders();
 	} elseif($_SESSION['adminLoggedIn']) && $answer == false){
-	    echo '<center><p style="color: red; font-size: 20px" class="loading">Currently no orders to process<span>.</span><span>.</span><span>.</span></p></center>';
+	    echo '<center><p style="font-size: 20px" class="loading">Currently no orders to process<span>.</span><span>.</span><span>.</span></p></center>';
 	    echo '<meta http-equiv="Refresh" content="15">';
 	} elseif(!isset($_SESSION['adminLoggedIn'])) {
 	    echo '<center><p style="color: red; font-size: 20px" class="loading">UNAUTHORIZED USER DETECTED<span>.</span><span>.</span><span>.</span></p></center>';
