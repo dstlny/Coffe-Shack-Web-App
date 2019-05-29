03c1537cc05b09ba385643ba76c8825d67be8fb4
Luke Elam <lukeelam1999@icloud.com>
2019-05-29 21:26:29 +0100

    Just some simple updates. Security related.

--- coffee-shack-source-oracle/php/login.php
+++ coffee-shack-source-oracle/php/login.php
@@ -8,28 +8,34 @@ if (isset($_POST['subLogin'])) {
     $hash = hash('sha512', $pass).hash('sha384', $pass).hash('whirlpool', $pass);
     
     if(empty($mailuid) && empty($pass)){
+
         $_SESSION['errors']['loginEmpty'] = "<br><b style=\"color: red; font-size: 12px;\">Login fields cannot be empty!</b>";
         oci_free_statement($result);
-	oci_close($connection);
-	header("location: ../pages/home.php");
+        oci_close($connection);
+        header("location: ../pages/home.php");
         exit();
+
     } else{
         if(empty($mailuid)){
+
             $_SESSION['errors']['email'] = "<b style=\"color: red; font-size: 12px;\">Please enter a email address!</b>";
             oci_free_statement($result);
-	    oci_close($connection);
-	    header("location: ../pages/home.php");
+            oci_close($connection);
+            header("location: ../pages/home.php");
             exit();
+
         } elseif(empty($pass)){
+
             $_SESSION['errors']['pass'] = "<b style=\"color: red; font-size: 12px;\">Please enter a password!</b>";
             oci_free_statement($result);
-	    oci_close($connection);
-	    header("location: ../pages/home.php");
+            oci_close($connection);
+            header("location: ../pages/home.php");
             exit();
+
         } else {
         
          //check customer exists
-         $query  = "SELECT * FROM CUSTOMER WHERE EmailAddress = '$mailuid'";
+         $query  = "SELECT PASSWORD FROM CUSTOMER WHERE EmailAddress = '$mailuid'";
          $result = oci_parse($connection, $query);
 		 
          if(oci_execute($result)){
@@ -40,8 +46,8 @@ if (isset($_POST['subLogin'])) {
                     $_SESSION['loggedIn'] = TRUE;
                     $_SESSION['userName'] = $row['EMAILADDRESS'];
                     $_SESSION['userID'] = $row['USER_ID'];
-		    oci_free_statement($result);
-		    oci_close($connection);
+                    oci_free_statement($result);
+                    oci_close($connection);
                     header("location: ../pages/home.php");
                     exit();
                     
@@ -49,63 +55,76 @@ if (isset($_POST['subLogin'])) {
                      
                     $_SESSION['errors']['pass'] = "<b style=\"color: red; font-size: 12px;\">Password doesn't match!</b>";
                     oci_free_statement($result);
-		    oci_close($connection);
+		            oci_close($connection);
                     header("location: ../pages/home.php");
                     exit();
                     
                  }
              } else{
+
                 //check staff/admin exists
-                $query  = "SELECT * FROM STAFF WHERE Email_Address = '$mailuid'";
+                $query  = "SELECT PASSWORD FROM STAFF WHERE Email_Address = '$mailuid'";
                 $result = oci_parse($connection, $query);
                 
                 if(oci_execute($result)){
                     if($row = oci_fetch_assoc($result)){
                         if($hash == $row['PASSWORD']){
+
                             $_SESSION['errors']['success'] = "<br><b style=\"color: green; font-size: 12px;\">Succesfully logged in!</b>";
                             $_SESSION['adminLoggedIn'] = TRUE;
                             $_SESSION['adminUserName'] = $row['EMAIL_ADDRESS'];
                             $_SESSION['adminID'] = $row['STAFF_ID'];
-        		    oci_free_statement($result);
-        		    oci_close($connection);
+                            oci_free_statement($result);
+                            oci_close($connection);
                             header("location: ../pages/home.php");
                             exit();
+
                         } else{
+
                             $_SESSION['errors']['pass'] = "<b style=\"color: red; font-size: 12px;\">Password doesn't match!</b>";
                             oci_free_statement($result);
-        		    oci_close($connection);
-        		    header("location: ../pages/home.php");
+                            oci_close($connection);
+                            header("location: ../pages/home.php");
                             exit();
+
                          }
                     } else{
+
                         //Customer nor Staff exists with this email
                         $_SESSION['errors']['noRecog'] = "<b style=\"color: red; font-size: 12px;\">Account not recognised!</b>";
                         oci_free_statement($result);
-        		oci_close($connection);
-        		header("location: ../pages/home.php");
+                        oci_close($connection);
+                        header("location: ../pages/home.php");
                         exit();
+
                     }
                 } else{
+
                     $_SESSION['errors']['sqlError'] = "<b style=\"color: red; font-size: 12px;\">An SQL error has occured!</b>";
                     oci_free_statement($result);
-        	    oci_close($connection);
-        	    header("location: ../pages/home.php");
+                    oci_close($connection);
+                    header("location: ../pages/home.php");
                     exit();
+                    
                 }
              }
            } else{
+
                $_SESSION['errors']['sqlError'] = "<b style=\"color: red; font-size: 12px;\">An SQL error has occured!</b>";
                oci_free_statement($result);
-	       oci_close($connection);
-	       header("location: ../pages/home.php");
+               oci_close($connection);
+               header("location: ../pages/home.php");
                exit();
+
            }
         }
     }
 } else{
+
     oci_free_statement($result);
     oci_close($connection);
     header("location: ../pages/home.php");
     exit();
+    
 }
 ?>
--- coffee-shack-source-oracle/php/myFunctions.php
+++ coffee-shack-source-oracle/php/myFunctions.php
@@ -7,6 +7,8 @@ class myFunctions {
        oci_execute($stid);
        $row = oci_fetch_assoc($stid);
        return $row['MAXID'];
+       oci_free_statement($stid);
+       oci_close($connection);
    }
 
    public function getOrderStatus($id){
@@ -15,6 +17,8 @@ class myFunctions {
       oci_execute($stid);
       $row = oci_fetch_assoc($stid);
       return $row['ORDER_COMPLETE'];
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function returnCurrID(){
@@ -23,6 +27,8 @@ class myFunctions {
       oci_execute($stid);
       $row = oci_fetch_assoc($stid);
       return $row['MAXID'];
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function step1($total,$current_timestamp,$table){
@@ -31,6 +37,8 @@ class myFunctions {
       $query = "INSERT INTO ORDERS VALUES(ORDERS_SEQ.nextval,$table,$total,TIMESTAMP '$current_timestamp','N',$userID,NULL)";
       $stid = oci_parse($connection, $query);
       oci_execute($stid);
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function step2($qty,$prod_id){
@@ -38,11 +46,13 @@ class myFunctions {
       $query = "INSERT INTO ORDER_ITEMS VALUES (ORDER_ITEMS_SEQ.nextval,$qty,$prod_id, ORDERS_SEQ.currval)";
       $stid = oci_parse($connection, $query);
       oci_execute($stid);
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function printItems($category){
       include '../dbcon/init.php';
-      $query  = "SELECT * FROM PRODUCT WHERE PRODUCT_CAT = '$category'";
+      $query  = "SELECT PRODUCT_NAME, PRICE, PRODUCT_IMAGE, PRODUCT_ID FROM PRODUCT WHERE PRODUCT_CAT = '$category'";
       $stid = oci_parse($connection, $query);
       oci_execute($stid);
       $i = 0;
@@ -81,18 +91,20 @@ class myFunctions {
       echo '              </table>';
       echo "\n       <!-- TABLE FOOTER END -->\n";
       echo "     <!-- TABLE HTML CODE END -->\n\n";
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function getUserDetails($email){
       include '../dbcon/init.php';
-      $query = "SELECT * FROM CUSTOMER WHERE EMAILADDRESS = '$email'";
+      $query = "SELECT EMAILADDRESS, USER_FORNAME, USER_SURNAME FROM CUSTOMER WHERE EMAILADDRESS = '$email'";
       $stid = oci_parse($connection, $query);
       oci_execute($stid);
       $row = oci_fetch_assoc($stid);
       $rowcount = oci_num_rows($stid);
 
       if($rowcount == 0){
-        $query  = "SELECT * FROM STAFF WHERE EMAIL_ADDRESS = '$email'";
+        $query  = "SELECT EMAIL_ADDRESS FROM STAFF WHERE EMAIL_ADDRESS = '$email'";
         $stid = oci_parse($connection, $query);
         oci_execute($stid);
         $row = oci_fetch_assoc($stid);
@@ -101,7 +113,7 @@ class myFunctions {
       } else{
         echo "<div class=\"userDetails\"><form><fieldset><legend>Stored details</legend><label for=\"Email\">Email</label><input type=\"text\" name=\"Email\" 
             value=\"{$row['EMAILADDRESS']}\" readonly><br><hr><label for=name\">Fullname</label><input type=\"text\" name=\"name\" value=\"{$row['USER_FORNAME']} {$row['USER_SURNAME']}\" readonly></fieldset></form>";
-            echo  '<center><p style="font-size:13px;">Want to logout? <a href="../php/logout.php" style="font-size:13px;">Logout</a></p></center></div>';
+        echo  '<center><p style="font-size:13px;">Want to logout? <a href="../php/logout.php" style="font-size:13px;">Logout</a></p></center></div>';
       }
 
       oci_free_statement($stid);
@@ -115,16 +127,15 @@ class myFunctions {
           $_SESSION['orderID'] = $this->returnNextID();
       }
 
-      echo '<div class="userDetails" style="margin-top: 0px;"><div>';
-      echo "<p><p style=\"font-size: 15px;\">Order ID: {$_SESSION['orderID']}</p><br>Please select your table number: </p><form method=\post\ action=\../pages/payment.php\">
-      <select name=\"tblNo\">";
-	 for($i=1; $i < 15; $i++){
-	     echo "<option value=\"{$i}\">{$i}</option>";
-	 }
+      echo "<div class=\"userDetails\" style=\"margin-top: 0px;\"><div><p><p style=\"font-size: 15px;\">Order ID: {$_SESSION['orderID']}</p><br>Please select your table number: </p><form method=\"post\" action=\"../pages/payment.php\"><select name=\"tblNo\">";
+      for($i=1; $i < 15; $i++){
+          echo "<option value=\"{$i}\">{$i}</option>";
+      }
       echo '</select>'; 
+
       for($i = 0; $i < count($_SESSION['mainOrder']); $i++){
         foreach($_SESSION['mainOrder'][$i] as $key=>$value){
-         $query = "SELECT * FROM PRODUCT WHERE PRODUCT_ID = $key";
+         $query = "SELECT PRODUCT_NAME, PRICE FROM PRODUCT WHERE PRODUCT_ID = $key";
          $stid = oci_parse($connection, $query);
          oci_execute($stid);
           while($row = oci_fetch_assoc($stid)) {
@@ -136,7 +147,7 @@ class myFunctions {
 
       for($i = 0; $i < count($_SESSION['sideOrder']); $i++){
         foreach($_SESSION['sideOrder'][$i] as $key=>$value){
-         $query = "SELECT * FROM PRODUCT WHERE PRODUCT_ID = $key";
+         $query = "SELECT PRODUCT_NAME, PRICE FROM PRODUCT WHERE PRODUCT_ID = $key";
          $stid = oci_parse($connection, $query);
          oci_execute($stid);
            while($row = oci_fetch_assoc($stid)) {
@@ -146,8 +157,9 @@ class myFunctions {
         }
       }
       $_SESSION['orderTotal'] = $total;
-      echo '<br><center><a style="color: black;" href="?ClearAll">Remove all</a></center><p style="font-size: 15px; border-right: 1px solid black; border-left: 1px solid black;"><b>Total: £'.number_format($total, 2).'</b></p><button type="submit" class="buttonAsLink">Pay Now!</button><br><br>';
-      echo '</div></div>';
+      echo '<br><center><a style="color: black;" href="?ClearAll">Remove all</a></center><p style="font-size: 15px; border-right: 1px solid black; border-left: 1px solid black;"><b>Total: £'.number_format($total, 2).'</b></p><button type="submit" class="buttonAsLink">Pay Now!</button><br><br></div></div>';
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function printFinal(){
@@ -156,7 +168,7 @@ class myFunctions {
       echo '<div class="basket"><div>';
       for($i = 0; $i < count($_SESSION['mainOrder']); $i++){
         foreach($_SESSION['mainOrder'][$i] as $key=>$value){
-         $query = "SELECT * FROM PRODUCT WHERE PRODUCT_ID  = $key";
+         $query = "SELECT PRODUCT_NAME, PRICE FROM PRODUCT WHERE PRODUCT_ID  = $key";
          $stid = oci_parse($connection, $query);
          oci_execute($stid);
            while($row = oci_fetch_assoc($stid)) {
@@ -168,7 +180,7 @@ class myFunctions {
 
       for($i = 0; $i < count($_SESSION['sideOrder']); $i++){
         foreach($_SESSION['sideOrder'][$i] as $key=>$value){
-         $query = "SELECT * FROM PRODUCT WHERE PRODUCT_ID = $key";
+         $query = "SELECT PRODUCT_NAME, PRICE FROM PRODUCT WHERE PRODUCT_ID = $key";
          $stid = oci_parse($connection, $query);
          oci_execute($stid);
            while($row = oci_fetch_assoc($stid)) {
@@ -177,8 +189,9 @@ class myFunctions {
            }
         }
       }
-    echo "<p style=\"font-size: 15px; border-right: 1px solid black; border-left: 1px solid black;\"><b>Total: &pound;".number_format($total, 2)."</b></p>";
-    echo '</div></div>';
+    echo "<p style=\"font-size: 15px; border-right: 1px solid black; border-left: 1px solid black;\"><b>Total: &pound;".number_format($total, 2)."</b></p></div></div>";
+    oci_free_statement($stid);
+    oci_close($connection);
   }
 	
   public function checkOrders(){
@@ -191,21 +204,18 @@ class myFunctions {
       oci_execute($stid);
 
       while($row = oci_fetch_assoc($stid)) {
-	    $status = true;
+	      $status = true;
       }
       
-     if($status){
-	return true;
-     } elseif(!$status){
-     	return false;
-     }
-	   
+      return $status;
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function printCustomerOrders(){
       include '../dbcon/init.php';
  	  
-      $query = "SELECT * FROM ORDERS WHERE ORDER_COMPLETE = 'N'";
+      $query = "SELECT ORDER_ID, ORDER_TOTAL FROM ORDERS WHERE ORDER_COMPLETE = 'N'";
       $stid = oci_parse($connection, $query);
       oci_execute($stid);
       echo '<div class="staff-wrapper">';
@@ -215,33 +225,39 @@ class myFunctions {
         <input type=\"hidden\" name=\"orderID\" value=\"{$row['ORDER_ID']}\">
         <input type=\"hidden\" name=\"staffID\" value=\"{$_SESSION['adminID']}\">
         <h3 style=\"font-size: 16px; font-weight: bold;\">Order {$row['ORDER_ID']}&emsp;&emsp;Total: &pound;".number_format($row['ORDER_TOTAL'],2)."<br>[Not complete]</h3>
-        <hr>
-        <div>";
+        <hr><div>";
         $this->getOrderItemsFromOrderID($row['ORDER_ID']);
         echo '<button type="submit" style="font-size:16px; margin-bottom:15px;" name="subOrder" class="buttonAsLink">Complete Order</button></form></div>
         </div>';
       }
       echo '</div>';
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function getOrderItemsFromOrderID($id){
       include '../dbcon/init.php';
-      $query = "SELECT * FROM ORDER_ITEMS WHERE FK2_ORDER_ID = $id";
+      $query = "SELECT FK1_PRODUCT_ID, QUANTITY FROM ORDER_ITEMS WHERE FK2_ORDER_ID = $id";
       $stid = oci_parse($connection, $query);
       oci_execute($stid);
-      while($row = oci_fetch_assoc($stid)) 
+      while($row = oci_fetch_assoc($stid)){
         echo "<p style=\"line-height: 0.5;   font-size: 10pt\">{$row['QUANTITY']} x {$this->getProductFromForeignKey($row['FK1_PRODUCT_ID'])}</p>";
+      }
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 
   public function getProductFromForeignKey($id){
       include '../dbcon/init.php';
-      $query = "SELECT * FROM PRODUCT WHERE PRODUCT_ID = $id";
+      $query = "SELECT PRODUCT_NAME FROM PRODUCT WHERE PRODUCT_ID = $id";
       $stid = oci_parse($connection, $query);
       oci_execute($stid);
-      while($row = oci_fetch_assoc($stid)) 
+      while($row = oci_fetch_assoc($stid)){
         return $row['PRODUCT_NAME'];
-
+      }
       $this->printCustomerOrders();
+      oci_free_statement($stid);
+      oci_close($connection);
   }
 	
 }
